61b3e03b4608790805692bac6ffbbf0d77c3c6c6
wyrde <wyrde@users.noreply.github.com>
2023-05-06 20:27:50 -0400

    configurations

---
+++ docs/conf.py
@@ -0,0 +1,20 @@
+# conf.py
+# https://docs.readthedocs.io/en/stable/config-file/v2.html
+# https://docs.readthedocs.io/en/stable/guides/migrate-rest-myst.html
+
+
+extensions = [
+    # Your existing extensions
+    ...,
+    "myst_parser",
+]
+myst_enable_extensions = [
+     "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "substitution"]
